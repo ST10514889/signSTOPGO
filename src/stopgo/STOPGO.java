932406153ad6f02a20c683648fe12b3dfafb0a07
ST10514889 <ST10514889@myemeris.edu.za>
2026-02-25 14:19:46 +0200

    Final submission

---
+++ src/stopgo/STOPGO.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package stopgo;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class STOPGO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("                   ------------------------\n "
+            + "       ***********                                  xxxxxxxxxx\n "
+            + "      *           *                                x          x\n     "
+            
+                + "\n"
+                + "     *    s t o p     *                          x       GO       x\n"
+                + "\n"
+                + "      *              *                            x             x\n"
+                + "\n"
+                + "        ***********                                  xxxxxxxxxx \n"
+                + "             11                                          11\n"
+                + "             11                                          11\n"
+                + "             11                                          11\n"
+                + "             11                                          11\n"
+                + "           111111                                      111111\n"
+                + "       11111111111111                               111111111111\n"
+   + "|-------------------------------------------------------------------------------------------------------------|\n"
+   + "|-------------------------------------------------------------------------------------------------------------|\n    ");
+                                                                      
+                                                                    
+    }//Stop and go 
+    
+}                  
